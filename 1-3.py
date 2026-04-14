f0c52485fd7a51ea00ca21ae688b438ab642db84
sian <lij891225@gmail.com>
2026-04-14 16:38:46 +0800

    新增1-3 1-4

---
+++ 1-3.py
@@ -0,0 +1,9 @@
+str1 = input()
+str2 = input()
+str3 = input()
+str4 = input()
+
+print(f"|{str1:>10s} {str2:>10s}|")
+print(f"|{str3:>10s} {str4:>10s}|")
+print(f"|{str1:10s} {str2:10s}|")
+print(f"|{str3:10s} {str4:10s}|")
