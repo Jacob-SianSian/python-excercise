b79b6674b9d07467eb86663e2615d19dea735607
sian <lij891225@gmail.com>
2026-04-14 10:54:09 +0800

    新增1-1 1-2題目

---
+++ 1-1.py
@@ -0,0 +1,9 @@
+num1 = eval(input())
+num2 = eval(input())
+num3 = eval(input())
+num4 = eval(input())
+
+print(f"|{num1:5d} {num2:5d}|")
+print(f"|{num3:5d} {num4:5d}|")
+print(f"|{num1:<5d} {num2:<5d}|")
+print(f"|{num3:<5d} {num4:<5d}|")
