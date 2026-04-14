4e44bc5ccce2ff213519bab494c5e9b1605e0e04
sian <lij891225@gmail.com>
2026-04-14 16:48:25 +0800

    修改1-4 新增1-5

---
+++ 1-4.py
@@ -0,0 +1,7 @@
+import math
+radius = eval(input())
+area = math.pi * radius ** 2
+perimeter = 2 * math.pi * radius
+print(f"Radius = {radius:.2f}")
+print(f"Perimeter = {perimeter:.2f}")
+print(f"Area = {area:.2f}")
---
+++ 1-5.py
@@ -0,0 +1,8 @@
+height = eval(input())
+width = eval(input())
+perimeter = 2 * (height + width)
+area = height * width
+print(f"Height = {height:.2f}")
+print(f"Width = {width:.2f}")
+print(f"Perimeter = {perimeter:.2f}")
+print(f"Area = {area:.2f}")
