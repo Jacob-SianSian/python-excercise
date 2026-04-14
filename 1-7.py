d38a5c6207fe1d559bef73aacf18287d10e4fcb0
sian <lij891225@gmail.com>
2026-04-14 16:58:40 +0800

    新增1-7

---
+++ 1-7.py
@@ -0,0 +1,11 @@
+num1 = eval(input())
+num2 = eval(input())
+num3 = eval(input())
+num4 = eval(input())
+num5 = eval(input())
+
+sum = num1 + num2 + num3 + num4 + num5
+average = sum / 5
+print(num1, num2, num3, num4, num5)
+print(f"Sum = {sum:.1f}")
+print(f"Average = {average:.1f}")
